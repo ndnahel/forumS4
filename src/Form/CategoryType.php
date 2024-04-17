dafcba36d4564c0a37018d3acb0e6e10211b8ec3
Nahel <72753978+Nahelv@users.noreply.github.com>
2024-04-17 16:26:37 +0200

    fix form style

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,24 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+				'label' => 'Nom',
+				'label_attr' => [
+					'class' => 'form-label'
+				],
+	            'attr' => [
+	                'placeholder' => 'Nom de la catégorie',
+		            'class' => 'form-control mb-2'
+	            ]
+            ])
             ->add('color', ColorType::class, [
 				'label' => 'Couleur',
+				'label_attr' => [
+					'class' => 'form-label'
+				],
+	            'attr' => [
+	                'class' => 'form-control'
+	            ]
 			])
         ;
     }
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -8,6 +8,8 @@ use App\Entity\Tags;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,12 +18,39 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('message')
+            ->add('title', TextType::class, [
+				'label' => 'Titre',
+				'label_attr' => [
+					'class' => 'form-label'
+				],
+				'attr' => [
+					'placeholder' => 'Titre du message',
+					'class' => 'form-control mb-2'
+				]
+			])
+            ->add('message', TextareaType::class,
+	            [
+					'label' => 'Message',
+		            'label_attr' => [
+						'class' => 'form-label'
+		            ],
+		            'attr' => [
+						'placeholder' => 'Contenu du message',
+			            'class' => 'form-control mb-2'
+		            ]
+	            ]
+            )
             ->add('tags', EntityType::class, [
                 'class' => Tags::class,
                 'choice_label' => 'libelle',
                 'multiple' => true,
+	            'label' => 'Tags',
+	            'label_attr' => [
+					'class' => 'form-label'
+	            ],
+	            'attr' => [
+					'class' => 'form-control mb-2 d-flex flex-wrap'
+	            ],
             ])
         ;
     }
